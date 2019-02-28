f5d0e37b0afd0975e6383cfb94964d73c9ee4f60
raymondgrc <raymond_grc@yahoo.com>
2019-02-28 14:57:58 -0600

    Remove app.css, add axios, add api call

--- randomcountry/src/App.js
+++ randomcountry/src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    countries: []
+  }
+
+  componentDidMount(){
+    axios.get('https://restcountries.eu/rest/v2/all')
+    .then((res) => {this.setState({countries: res.data})})
+    .catch((err) => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
