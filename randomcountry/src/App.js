9de874ffa625a9740578df8a5282718cd005d4bb
raymondgrc <raymond_grc@yahoo.com>
2019-03-01 01:33:03 -0600

    Add render random country name

--- randomcountry/src/App.js
+++ randomcountry/src/App.js
@@ -3,19 +3,26 @@ import axios from 'axios';
 
 class App extends Component {
   state = {
-    countries: []
+    randomCountryIndex: 0
+
   }
+  generateRandomNumber = (limit) => Math.floor(Math.random() * Math.floor(limit));
 
   componentDidMount(){
     axios.get('https://restcountries.eu/rest/v2/all')
-    .then((res) => {this.setState({countries: res.data})})
+    .then((res) => {
+      this.setState({
+        countries: res.data,
+        randomCountryIndex: this.generateRandomNumber(res.data.length)
+      });
+    })
     .catch((err) => console.log(err))
   }
 
   render() {
     return (
       <div className="App">
-        Hello World
+        {this.state.countries ? this.state.countries[this.state.randomCountryIndex].name : 'Country'}
       </div>
     );
   }
