daf4402ee6f438465222ae2a57b52531602ff9a9
raymondgrc <raymond_grc@yahoo.com>
2019-03-02 03:31:58 -0600

    Create map now in setstate callback, revamp country info code

--- randomcountry/src/App.js
+++ randomcountry/src/App.js
@@ -3,52 +3,41 @@ import axios from 'axios';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
-  state = {
-    randomCountryIndex: 0
-
-  }
+  state = {}
   generateRandomNumber = (limit) => Math.floor(Math.random() * Math.floor(limit));
-
+  createMap = () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: this.state.countries[this.state.randomCountryIndex].latlng[0], lng: this.state.countries[this.state.randomCountryIndex].latlng[1]},
+      zoom: 7,
+    });
+  }
   componentDidMount(){
     axios.get('https://restcountries.eu/rest/v2/all')
     .then((res) => {
       this.setState({
         countries: res.data,
         randomCountryIndex: this.generateRandomNumber(res.data.length)
-      });
-      let map = new window.google.maps.Map(document.getElementById('map'), {
-        center: {lat: this.state.countries[this.state.randomCountryIndex].latlng[0], lng: this.state.countries[this.state.randomCountryIndex].latlng[1]},
-        zoom: 7,
-      });
-      var geocoder = new window.google.maps.Geocoder();
-      geocoder.geocode( { 'address': this.state.countries[this.state.randomCountryIndex].name}, function(results, status) {
-        if (status == window.google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          map.fitBounds(results[0].geometry.viewport);
-        }
-      });
+      }, this.createMap);
     })
     .catch((err) => console.log(err))
   }
 
   render() {
-    let countryInfo = null;
-
+    let countryCard = null;
     if (this.state.countries) {
-      let condensedApi = this.state.countries;
-      let randomIndex = this.state.randomCountryIndex;
-      countryInfo = (
+      let {countries, randomCountryIndex} = this.state;
+      countryCard = (
         <div>
-          {condensedApi[randomIndex].name}
-          <img src={condensedApi[randomIndex].flag}/>
-      </div>
+          {countries[randomCountryIndex].name}
+          <img src={countries[randomCountryIndex].flag}/>
+        </div>
       )
     }
 
 
     return (
       <CssBaseline>
-        {countryInfo}
+        {countryCard}
         <div id="map" style={{height: '600px'}}/>
       </CssBaseline>
     );
